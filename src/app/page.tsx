9f0d1fda31bcbbb6b13641f5f2fedbc78e8f6c68
Kurl Angelo Palaganas <palaganaskurl@gmail.com>
2025-06-21 17:47:06 +0800

    feat: reproducer cloudflaresocket is not a constructor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
 import Image from "next/image";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-export default function Home() {
+export default async function Home() {
+  // Make sure to install the 'postgres' package
+
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+  const db = drizzle({ client: pool });
+
+  const result = await db.execute("select 1");
+
+  console.log("result", result);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
